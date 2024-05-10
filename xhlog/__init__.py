cb39cfaece12271d3bea2f6c69107d55919c488e
Yeying-Xingchen <yeyingxingchen@yeah.net>
2024-05-10 21:18:56 +0800

    修改 参数名称。

--- xhlog/__init__.py
+++ xhlog/__init__.py
@@ -1,25 +1,25 @@
 import datetime
 
-def info(log):
+def info(message):
     current_time = datetime.datetime.now()
     formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
-    print("\033[94m[" + formatted_time + "]\033[0m\033[92m[INFO]\033[0m" + log)
+    print("\033[94m[" + formatted_time + "]\033[0m\033[92m[INFO]\033[0m" + message)
     with open("./log/"+current_time.strftime("%Y-%m-%d") + '.log', 'a') as f:
-        f.write("[" + formatted_time + "][信息/INFO]" + log + "\n")
+        f.write("[" + formatted_time + "][信息/INFO]" + message + "\n")
 
-def warning(log):
+def warning(message):
     current_time = datetime.datetime.now()
     formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
-    print("\033[94m[" + formatted_time + "]\033[0m\033[91m[WARN]\033[0m" + log)
+    print("\033[94m[" + formatted_time + "]\033[0m\033[91m[WARN]\033[0m" + message)
     with open("./log/"+current_time.strftime("%Y-%m-%d") + '.log', 'a') as f:
-        f.write("[" + formatted_time + "][警告/WARN]" + log + "\n")
+        f.write("[" + formatted_time + "][警告/WARN]" + message + "\n")
 
-def error(log,exit=False):
+def error(message,exit=False):
     current_time = datetime.datetime.now()
     formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
-    print("\033[94m[" + formatted_time + "]\033[0m\033[31m[ERROR]\033[0m" + log)
+    print("\033[94m[" + formatted_time + "]\033[0m\033[31m[ERROR]\033[0m" + message)
     with open("./log/"+current_time.strftime("%Y-%m-%d") + '.log', 'a') as f:
-        f.write("[" + formatted_time + "][错误/ERROR]" + log + "\n")
+        f.write("[" + formatted_time + "][错误/ERROR]" + message + "\n")
     if exit==True:
         exit(1)
     else:
